9a96396569d274864c577c69f4f433eddaabea2a
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-09-29 18:36:17 +0600

    test commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import * as functions from "./modules/functions.js";
+
+functions.isWebp();
+
+// import Swiper, { Navigation, Pagination } from "swiper";
+
+console.log('js работает и в режиме разработки из файла app.js');
+
+//include('./script.js');
+
+import './script.js';
---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+console.log('script.js тоже подключен.');
+
+// scroll-background
+
+const header = document.querySelector('.header');
+const logo = document.querySelector('.header__logo');
+
+function toggleHeaderClass() {
+   if (window.scrollY > 0) {
+      header.classList.add('scroll');
+      logo.classList.add('scroll');
+   } else {
+      header.classList.remove('scroll');
+      logo.classList.remove('scroll');
+   }
+}
+
+window.addEventListener('scroll', toggleHeaderClass);
+
+
+// burger activation
+
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body'); // Получаем элемент <body>
+
+burger.addEventListener('click', () => {
+   burger.classList.toggle('active');
+   headerMenu.classList.toggle('active');
+
+   // Добавляем или убираем класс, чтобы заблокировать/разблокировать скролл
+   body.classList.toggle('scroll-lock');
+});
+
+// Событие при нажатии на ссылку в меню
+headerMenu.addEventListener('click', (event) => {
+   // Проверяем, является ли клик на ссылку
+   if (event.target.tagName === 'A') {
+      // Сворачиваем бургер и меню
+      burger.classList.remove('active');
+      headerMenu.classList.remove('active');
+      body.classList.remove('scroll-lock');
+   }
+});
