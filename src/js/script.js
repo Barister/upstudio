346c52c1a58910ad13fcb24770da134447357319
Evgeny Liskevich <59793749+Barister@users.noreply.github.com>
2023-10-12 17:16:31 +0600

    Done project slider in pure js and data from file

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,157 @@ headerMenu.addEventListener('click', (event) => {
 });
 
 
+
+
+// загрузка items в portfolio
+
+const portfolio = document.querySelector('.portfolio');
+const portfolioHeader = document.querySelector('.portfolio__header');
+let portfolioBody;
+
+
+console.log('portfolioBody:', portfolioBody);
+
+// content-portfolio__title = projectTitle
+// item-content__subject h5 = projectMainFont
+// content-portfolio__item img = projectColors
+// slider-portfolio__preview = projectImage
+// slider-portfolio__view = projectLink
+
+if (portfolio) {
+   loadPortfolioItems();
+}
+
+let counter = 0;
+
+
+
+
+
+console.log('counter:', counter);
+
+// function changePortfolioItem() {
+//    if 
+// }
+
+
+async function loadPortfolioItems() {
+   const response = await fetch("files/portfolio.json", {
+      method: "GET"
+   });
+
+   if (response.ok) {
+      const responseResult = await response.json();
+      console.log('responseResult:', responseResult.projects[counter]);
+      initPortfolio(responseResult.projects[counter]);
+      let projectsLength = responseResult.projects.length;
+      console.log('projectsLength:', projectsLength);
+
+      const previousProjectBtn = document.querySelector('.footer-slider__left');
+      const nextProjectBtn = document.querySelector('.footer-slider__right');
+
+      previousProjectBtn.addEventListener('click', () => {
+         console.log('click left');
+         if (counter > 0) {
+            counter -= 1;
+            loadPortfolioItems();
+         }
+      });
+
+      nextProjectBtn.addEventListener('click', () => {
+         console.log('click right');
+         if (counter < projectsLength - 1) {
+            counter += 1;
+            loadPortfolioItems();
+         }
+      });
+
+      console.log('counter:', counter);
+   }
+   else {
+      alert("Error");
+   }
+
+
+}
+
+
+function initPortfolio(project) {
+
+   buildPortfolioProject(project);
+
+}
+
+
+function buildPortfolioProject(project) {
+   const portfolioProjectTemplate = `
+      <div class="portfolio__body body-portfolio">
+         <div class="body-portfolio__content content-portfolio">
+            <h4 class="content-portfolio__title">${project.title}</h4>
+            <div class="content-portfolio__item item-content">
+               <div class="item-content__subject">Типографика</div>
+               <h5>${project.font}</h5>
+               <p>Аа Бб Вв Гг Дд Ee Ёё Жж Зз Ии Кк Лл Мм Нн Оо Пп Рр Сс Тт Уу Фф Хх Цц Чч Шш Щщ ъ ы ь Ээ Юю Яя
+               </p>
+               <p>Аа Бб Вв Гг Дд Ee Ёё Жж Зз Ии Кк Лл Мм Нн Оо Пп Рр Сс Тт Уу Фф Хх Цц Чч Шш Щщ ъ ы ь Ээ Юю Яя
+               </p>
+            </div>
+            <div class="content-portfolio__item item-content">
+               <div class="item-content__subject">цвета</div>
+               <img src="${project.colors}" alt="site colours"></img>
+            </div>
+         </div>
+         <div class="body-porfolio__slider slider-portfolio">
+            <div class="slider-portfolio__content">
+               <img src="${project.image}" alt="template preview" class="slider-portfolio__preview">
+               <a href="${project.url}" class="slider-portfolio__view btn btn--white" target="_blank">Перейти</a>
+            </div>
+            <div class="slider-portfolio__footer footer-slider">
+               <button class="footer-slider__left">
+                  <img src="img/portfolio/control.svg" alt="Slider Control">
+               </button>
+               <button class="footer-slider__button btn btn--white btn--bullet"><span>Заказать
+                     сайт</span></button>
+               <button class="footer-slider__right">
+                  <img src="img/portfolio/control.svg" alt="Slider Control">
+               </button>
+            </div>
+         </div>
+      </div>
+   `;
+   if (portfolioBody) {
+
+      portfolioBody.innerHTML = '';
+   }
+   portfolioHeader.insertAdjacentHTML('afterend', portfolioProjectTemplate);
+
+   portfolioBody = document.querySelector('.portfolio__body');
+}
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+//    const previousProjectBtn = document.querySelector('.footer-slider__left');
+//    const nextProjectBtn = document.querySelector('.footer-slider__right');
+
+//    previousProjectBtn.addEventListener('click', () => {
+
+//       console.log('click left');
+
+//       if (counter > 0) {
+//          counter -= 1;
+//          loadPortfolioItems();
+//       }
+//    });
+
+//    nextProjectBtn.addEventListener('click', () => {
+
+//       console.log('click right');
+
+//       if (counter < projectsLength) {
+//          counter += 1;
+//          loadPortfolioItems();
+//       }
+//    });
+
+// });
